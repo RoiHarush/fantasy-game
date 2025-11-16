28e0563fba34eab96b36e88c08049b405ae5471e
Roi Harush <roi.harush12@gmail.com>
2025-11-16 10:36:55 +0200

    Delete frontend directory

--- frontend/src/services/pointsService.js
+++
@@ -1,7 +0,0 @@
-import API_URL from "../config";
-
-export async function fetchUserPoints(userId, gameweekId) {
-    const res = await fetch(`${API_URL}/api/points/${userId}/${gameweekId}`);
-    if (!res.ok) throw new Error("Failed to fetch points");
-    return res.json();
-}
