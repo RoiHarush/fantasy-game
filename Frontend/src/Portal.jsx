6122d1aed0cd77b5d75b168e45ade76ac82e6bf7
Roi Harush <roi.harush12@gmail.com>
2025-11-16 10:11:54 +0200

    Remove old Frontend folder after rename

--- Frontend/src/Portal.jsx
+++
@@ -1,6 +0,0 @@
-import { createPortal } from "react-dom";
-
-export default function Portal({ children }) {
-    const modalRoot = document.getElementById("modal-root") || document.body;
-    return createPortal(children, modalRoot);
-}
